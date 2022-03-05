70bdee7f1b40011a23adcdebb70db9e4c451bf6e
Renato Oliveira <oliversieto@gmail.com>
2022-03-05 08:14:31 -0400

    feat: start a server with mux

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	fmt.Println("Server started on port 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
