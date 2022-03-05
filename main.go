2a5236f410dbe7879e234aa4d6902e08eb280e99
Renato Oliveira <oliversieto@gmail.com>
2022-03-05 08:41:48 -0400

    feat: create route for add users

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"basic-crud/user"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-
+	router.HandleFunc("/users", user.CreateUser).Methods("POST")
 	fmt.Println("Server started on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
